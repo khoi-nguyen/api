407e4ab9ef1127988e5749c2cbd1031098386582
Khoi Nguyen <khoi@nguyen.me.uk>
2024-05-04 10:03:51 +0200

    Move some files into core

--- query/expression.py
+++ query/expression.py
@@ -2,29 +2,9 @@ import strawberry
 import sympy
 import sympy.parsing.latex
 import typing
-
-
-def parse_latex(expr: str):
-    parsed = sympy.parsing.latex.parse_latex(expr)
-    subs = {
-        sympy.Symbol("e"): sympy.E,
-        sympy.Symbol("i"): sympy.I,
-        sympy.Symbol("pi"): sympy.pi,
-    }
-    return parsed.subs(subs) if parsed else None
-
-
-Math = strawberry.scalar(
-    typing.NewType("MathExpression", sympy.Basic),
-    serialize=sympy.latex,
-    parse_value=parse_latex,
-)
+from core.math import as_math, Math
 
 x = typing.cast(Math, sympy.Symbol("x"))
-
-
-def as_math(x):
-    return typing.cast(Math, x)
 
 
 @strawberry.type
