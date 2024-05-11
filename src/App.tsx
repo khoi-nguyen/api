5250cb9081c27848d912a41fca8f8724af9126dd
Khoi Nguyen <khoi@nguyen.me.uk>
2024-05-11 18:35:27 +0200

    Change default value

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,19 @@ import { createSignal, type Component } from "solid-js";
 import Editor from "./components/Editor";
 import Markdown from "./components/Markdown";
 
+const value: string = `# Hello
+
+Here is a math formula:
+
+$$
+\\int_a^b f'(x)
+$$
+
+::factorise
+`;
+
 const App: Component = () => {
-  const [code, setCode] = createSignal("# Hello");
+  const [code, setCode] = createSignal(value);
   return (
     <div class="columns-2">
       <Editor initialValue={code()} onChange={setCode} />
