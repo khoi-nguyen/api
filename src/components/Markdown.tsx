7b59bcf750f7a7b6b3c9401866096624120a0b7c
Khoi Nguyen <khoi@nguyen.me.uk>
2024-05-11 18:10:36 +0200

    Math support for markdown

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -1,8 +1,15 @@
 import { type Component } from "solid-js";
 import { micromark } from "micromark";
+import { math, mathHtml } from "micromark-extension-math";
+
+import "katex/dist/katex.min.css";
 
 const Markdown: Component<{ value?: string }> = (props) => {
-  const html = () => micromark(props.value || "");
+  const html = () =>
+    micromark(props.value || "", {
+      extensions: [math()],
+      htmlExtensions: [mathHtml()],
+    });
   return <div innerHTML={html()} class="prose" />;
 };
 
