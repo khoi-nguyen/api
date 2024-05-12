6cff1af2c30a786bb394ddb8e0dddf83ab381c7f
Khoi Nguyen <khoi@nguyen.me.uk>
2024-05-12 15:03:23 +0200

    Use store instead

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import { type Component } from "solid-js";
+import { createStore } from "solid-js/store";
 import Document from "./components/Node";
 import { NodeType } from "./schema";
 
-const document: NodeType = {
-  component: "Page",
-  children: [
-    {
-      component: "Markdown",
-      props: {
-        value: "# Hello $x^2$ hello",
-      },
-    },
-    {
-      component: "Markdown",
-      props: {
-        value: "Test **strong**",
-      },
-    },
-  ],
-};
-
 const App: Component = () => {
+  const [document, setDocument] = createStore<NodeType>({
+    component: "Page",
+    children: [
+      {
+        component: "Markdown",
+        props: {
+          value: "# Hello $x^2$ hello",
+        },
+      },
+      {
+        component: "Markdown",
+        props: {
+          value: "Test **strong**",
+        },
+      },
+    ],
+  });
   return <Document {...document} />;
 };
 
