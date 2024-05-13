390c09dc42c304fb04bc069ccb0a2d40d2f8f5fd
Khoi Nguyen <khoi@nguyen.me.uk>
2024-05-13 17:34:04 +0200

    Remove editor from Markdown component

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -1,7 +1,6 @@
-import { type Component, createSignal, Show } from "solid-js";
+import { type Component } from "solid-js";
 import { micromark } from "micromark";
 import { math, mathHtml } from "micromark-extension-math";
-import Editor from "./Editor";
 
 import "katex/dist/katex.min.css";
 import { SetStoreFunction } from "solid-js/store";
@@ -10,48 +9,12 @@ const Markdown: Component<{
   value?: string;
   setter?: SetStoreFunction<{ props: { value?: string } }>;
 }> = (props) => {
-  const [edit, setEdit] = createSignal(false);
   const html = () =>
     micromark(props.value || "", {
       extensions: [math()],
       htmlExtensions: [mathHtml()],
     });
-  return (
-    <div
-      tabindex={0}
-      onClick={() => setEdit(true)}
-      onKeyDown={(event) => {
-        if (event.key === "Enter" && !edit()) {
-          event.preventDefault();
-          setEdit(true);
-        }
-      }}
-    >
-      <Show
-        when={edit()}
-        fallback={
-          <div innerHTML={html()} onClick={() => setEdit(true)} class="prose" />
-        }
-      >
-        <Editor
-          value={props.value}
-          onChange={(newValue) => {
-            if (props.setter) {
-              props.setter("props", "value", newValue);
-            }
-          }}
-          onKeyDown={(event) => {
-            const enter =
-              (event.shiftKey || event.ctrlKey) && event.keyCode === 3;
-            const esc = event.keyCode === 9;
-            if (enter || esc) {
-              setEdit(false);
-            }
-          }}
-        />
-      </Show>
-    </div>
-  );
+  return <div innerHTML={html()} class="prose" />;
 };
 
 export default Markdown;
