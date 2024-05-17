e600e21fbbe1368bf7aebf6283f915c630126e8a
Khoi Nguyen <khoi@nguyen.me.uk>
2024-05-17 16:38:10 +0200

    Styling environment

--- src/components/nodes/Environment.tsx
+++ src/components/nodes/Environment.tsx
@@ -2,11 +2,23 @@ import { ParentNode } from "../Node";
 
 export default function Environment(props: ParentNode<{ title: string }>) {
   return (
-    <div>
-      <h3 class="bg-blue-950 text-white py-1 px-3 rounded-t-md font-sans">
+    <div class="border rounded-t-md m-4">
+      <h3
+        class="bg-blue-950 text-white py-1 px-3 font-sans"
+        contenteditable
+        onKeyDown={(event) => {
+          if (event.key === "Enter") {
+            event.currentTarget.blur();
+          }
+        }}
+        onFocusOut={(event) => {
+          props.setter("props", "title", event.currentTarget.innerText);
+          event.preventDefault();
+        }}
+      >
         {props.title}
       </h3>
-      {props.children}
+      <div class="p-3">{props.children}</div>
     </div>
   );
 }
