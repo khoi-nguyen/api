35062421dfde6aeab98ee215ea325698ce1e0f18
Khoi Nguyen <khoi@nguyen.me.uk>
2024-05-17 11:48:53 +0200

    Center page

--- src/components/nodes/Page.tsx
+++ src/components/nodes/Page.tsx
@@ -2,7 +2,7 @@ import { ParentNode } from "../Node";
 
 export default function Page(props: ParentNode<{ title: string }>) {
   return (
-    <main class="m-4 h-[297mm] w-[210mm] overflow-hidden rounded-md bg-white p-8 shadow-lg">
+    <main class="my-4 mx-auto h-[297mm] w-[210mm] overflow-hidden rounded-md bg-white p-8 shadow-lg">
       <h1
         class="text-4xl"
         contenteditable
