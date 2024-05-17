654acfbbdbc1b5a90b0085e135a6c2a5b6978a6b
Khoi Nguyen <khoi@nguyen.me.uk>
2024-05-17 17:19:11 +0200

    Change title appearance

--- src/components/nodes/Page.tsx
+++ src/components/nodes/Page.tsx
@@ -4,7 +4,7 @@ export default function Page(props: ParentNode<{ title: string }>) {
   return (
     <main class="my-4 mx-auto h-[297mm] w-[210mm] overflow-hidden rounded-md bg-white p-8 shadow-lg">
       <h1
-        class="text-4xl"
+        class="text-5xl font-bold text-center mb-4"
         contenteditable
         onKeyDown={(event) => {
           if (event.key === "Enter") {
