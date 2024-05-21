718e06ef0067e02791e7098b15a28ce15cc19a8a
Khoi Nguyen <khoi@nguyen.me.uk>
2024-05-21 10:09:31 +0200

    Allow to read and write JSON files

--- api.py
+++ api.py
@@ -1,8 +1,11 @@
+import typing
 import fastapi
 from fastapi.middleware.cors import CORSMiddleware
+import pydantic
 import strawberry
 import strawberry.fastapi
 import query
+import json
 
 
 schema = strawberry.Schema(query=query.Query)
@@ -12,6 +15,7 @@ origins = [
     "https://nguyen.me.uk",
     "https://ngy.ecam.be",
     "http://localhost:3000",
+    "http://localhost:3001",
     "http://localhost",
 ]
 
@@ -23,3 +27,31 @@ app.add_middleware(
     allow_headers=["*"],
 )
 app.include_router(graphql_app, prefix="/graphql")
+
+
+def get_path(url: str) -> str:
+    path = f"data/documents/{url}"
+    if path.endswith("/"):
+        path += "index"
+    path += ".json"
+    return path
+
+
+class Node(pydantic.BaseModel):
+    component: str
+    props: typing.Dict[str, typing.Any]
+    children: typing.Optional[typing.List["Node"]] = None
+
+
+@app.get("/documents/{url}")
+async def read_document(url: str):
+    path = get_path(url)
+    with open(path, "r") as file:
+        return json.load(file)
+
+
+@app.post("/documents/{url}")
+async def write_document(url: str, document: Node):
+    path = get_path(url)
+    with open(path, "w") as file:
+        json.dump(document.model_dump(mode="json"), file, indent=2)
